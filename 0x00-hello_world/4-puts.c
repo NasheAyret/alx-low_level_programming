663ca45b4f003601aaac1b3f125e0bb8414132f0
NasheAyret <teryat256@gmail.com>
2022-04-11 16:30:59 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-nclude <stdio.h>
+#include <stdio.h>
 
 /**
-   * main - entry point
-    * Description: Print a string using followed by a new line
-     * Return: 0
-      */
+  * main - entry point
+  * Description: Print a string using followed by a new line
+  *Return:0
+  */
 int main(void)
 {
-	  puts("\"Programming is like building a multilingual puzzle");
-	    return (0);
-}
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}	
