d651a215ad7e5ce07fe88c75c525fe73f4949851
NasheAyret <teryat256@gmail.com>
2022-04-19 08:22:39 -0700

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,33 +7,18 @@
 
 int main(void)
 {
-<<<<<<< HEAD
-int i;
+        int i;
 
-=======
-	int 1:
-	
->>>>>>> 79ecab8b90f4ad90b306389879d3a7defc532fd1
-	printf("Infinite loop incoming :(\n");
-	
-	i = 0;
-<<<<<<< HEAD
+        printf("Infinite loop incoming :(\n");
 
-	/*
-	  * while (i < 10);
-	  * {
-	  * putchar(i);
-	  * }
-	  */
-=======
-	
-	/*while (i < 10)*/
-	/*{*/
-	/*putchar(i);*/
-	/*}*/
-	
->>>>>>> 79ecab8b90f4ad90b306389879d3a7defc532fd1
-	printf("Infinite loop avoided! \\o/\n");
-	
-	return (0);
+        i = 0;
+
+        while (i < 10)
+        {
+                putchar(i);
+        }
+
+        printf("Infinite loop avoided! \\o/\n");
+
+        return (0);
 }
