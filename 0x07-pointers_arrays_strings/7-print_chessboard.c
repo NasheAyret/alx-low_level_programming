19589542df4caee80e716dd22db3b88ed58d2cc3
NasheAyret <teryat256@gmail.com>
2022-04-25 11:44:21 -0700

    task 7

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* print_chessboard - prijts chessboard
+* print_chessboard - prints chessboard
 *
 * @a: rows
 * Return: nothing
@@ -12,10 +12,11 @@ int i, j;
 
 for (i = 0; i < 8; i++)
 {
+
 for (j = 0; j < 8; j++)
 {
 _putchar(a[i][j]);
 }
-putchar('\n');
+_putchar('\n');
 }
 }
