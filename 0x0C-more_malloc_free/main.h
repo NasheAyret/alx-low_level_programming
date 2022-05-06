4db92629156fe4acece82b987a4ed9d7db637741
NasheAyret <teryat256@gmail.com>
2022-05-06 13:22:06 -0700

    task 1

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,8 +1,3 @@
-#ifndef main.h
-#define main.h
-
-#include <stdlib.h>
-
 void _puts(char *str);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
@@ -11,6 +6,4 @@ int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
 int _atoi(const char *s);
-void print_int(unsigned long int n);
-
-#endif
+void print_int(unsigned long int new_size);
