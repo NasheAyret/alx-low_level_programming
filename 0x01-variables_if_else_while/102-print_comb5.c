1215a772654c0575644f4bba5fabd8a34125f5fd
NasheAyret <teryat256@gmail.com>
2022-05-30 15:12:06 -0700

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,50 +1,48 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
+ * main - print pairs of double digit combos
  *
- * Return: Always 0 (success/correct)
+ * Description: print pairs of double digit combos
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
-
-
-
 {
-int first_nums;
-int second_nums;
+	int i, j, k, m;
 
-
-for (first_nums = 0; first_nums < 100 ; first_nums++)
-{
-for (second_nums = first_nums + 1 ; second_nums < 100; second_nums++)
-{
-
-putchar (first_nums / 10 + '0');
-putchar (first_nums % 10 + '0');
-
-
-putchar (' ');
-
-putchar (second_nums / 10 + '0');
-putchar (second_nums % 10 + '0');
-
-
-if (first_nums == 98 && second_nums == 99)
-{
-break;
-}
-
-
-putchar(',');
-putchar (' ');
-
-
-}
-
-}
-
-putchar('\n');
-
-
-return (0);
-
+	i = 48;
+	while (i < 58)
+	{
+		j = 48;
+		while (j < 58)
+		{
+			m = j + 1;
+			k = i;
+			while (k < 58)
+			{
+				while (m < 58)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(32);
+					putchar(k);
+					putchar(m);
+					if (i < 57 || j < 56 || k < 57 || m < 57)
+					{
+						putchar(44);
+						putchar(32);
+					}
+					m++;
+				}
+				m = 48;
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar(10);
+	return (0);
 }
